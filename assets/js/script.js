e020bab745cea13d4eaefb0ea00145987d4ca983
Amir <Amirs@prdxn.com>
2020-01-15 11:05:47 +0530

    Responsive styling added with Js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,37 @@
-/* Author: 
+$('.hamburger').click(function(e) {
+	e.preventDefault();
+	$this = $(this);
+	$this.siblings('.nav__list').toggleClass('block');
+	$this.children('.hamburger__line1').toggleClass('ham1');
+	$this.children('.hamburger__line2').toggleClass('ham3');
+	$this.children('.hamburger__line3').toggleClass('ham2');
+});
+var index = 0;
+var slide_length = $('.slide').length;
+$slide = $(".slide");
+// slide image when clicked on left arrow
+$(".banner__prev").click(function(event){
+	event.preventDefault();
+	if (index <= 0) {
+		index = slide_length-1;
+	} else{
+		index--;
+	}
+	changeSlide(index);
+});
 
-*/
+// slide image when clicked on right arrow
+$(".banner__next").click(function(event){
+	event.preventDefault();
+	if (index >= slide_length-1) {
+		index=0;
+	} else{
+		index++;
+	}
+	changeSlide(index);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function changeSlide(i) {
+	$slide.removeClass("active");
+	$slide.eq(i).addClass('active');
+}
